82b18cb602db6d44e50564af3c7b8805cc286eee
Christian Morris <christiangmorris@gmail.com>
2024-02-22 01:19:29 +0900

    established something with app.tsx and it connects to django/python backend!?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// where I found this stuff
+// https://www.youtube.com/watch?v=fBA-jaWab9k
 
-  return (
-    <>
+class App extends React.Component {
+  state = { details: [] };
+  componentDidMount(): void {
+    let data;
+    axios
+      .get("http://localhost:8000/shipper/api")
+      .then((res) => {
+        data = res.data;
+        this.setState({
+          details: data,
+        });
+      })
+      .catch((err) => {});
+  }
+
+  render() {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header> Data generated from Django </header>
+        <hr></hr>
+        {this.state.details.map((output, id) => (
+          <div>
+            <h1>Did it?</h1>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
